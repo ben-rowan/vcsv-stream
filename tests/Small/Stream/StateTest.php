e9a0115fb8a79b3ccafff0c5f39fc64ed139358d
Ben Rowan <me@ben-rowan.com>
2019-08-04 20:00:01 +0100

    Add missing test to be completed

---
+++ tests/Small/Stream/StateTest.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace BenRowan\VCsvStream\Tests\Small\Stream;
+
+use BenRowan\VCsvStream\Tests\Assets\AbstractTestCase;
+
+class StateTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        $this->markTestSkipped('This needs testing');
+    }
+
+    /**
+     * @test
+     */
+    public function iCanSetAndGetHeader(): void
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function iCanTellWhenAHeaderIsNotSet(): void
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function iCanTellWhenAHeaderIsSet(): void
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function iCanAddAndGetARecord(): void
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function iCanAddAndGetMultipleRecords(): void
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function iCanTellWhenARecordHasNotBeenAdded(): void
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function iCanTellWhenARecordHasBeenAdded(): void
+    {
+
+    }
+}
